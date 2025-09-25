1d81a2c11f8cb132ef9cbe5947e88dc7f3ab1a67
jgoldchoco <168396876+jgoldchoco@users.noreply.github.com>
2025-09-25 02:52:40 -0400

    Add files via upload

---
+++ P1Lab1_GoldstonJasmin.py
@@ -0,0 +1,10 @@
+#Each print statement starts on a new line
+print("Jasmin Goldston")
+print("9/24/25")
+print("P1Lab1")
+print("Introduction to python, input and output")
+
+#Input function for first and last name
+first_name = input("Enter your first name:")
+last_name = input ("Enter your last name:")
+print(f"hello, {first_name} {last_name}! Welcome to CTI-110")
