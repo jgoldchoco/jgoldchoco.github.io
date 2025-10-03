cec2720484582572345ca37d3bde6fe9235263f0
jgoldchoco <168396876+jgoldchoco@users.noreply.github.com>
2025-10-02 21:12:57 -0400

    Add files via upload

---
+++ P1HW1_GoldstonJasmin.py
@@ -0,0 +1,25 @@
+#----- Calculating Exponents -----
+print("-----Calculating Exponents----")
+
+base = int(input("Enter an integer as the base value: " ))
+exponent = int(input("Enter an interger as the exponent: "))
+
+#Perform exponentiation 
+result = base ** exponent
+print(f"\n{base} raised to the power of {exponent} is {result} !!")
+
+#-----Addition and Subtraction-----
+print("-----Addition and Subtraction-----")
+
+start = int(input("Enter a starting integer: "))
+to_add = int(input("Enter an integer to add: "))
+to_subtract = int(input("Enter an integer to subtract: "))
+
+#Perform calculation 
+final_result = start + to_add - to_subtract
+print(f"\n{start} + {to_add} - {to_subtract} is equal to {final_result}")
+
+print("Jasmin Goldston")
+print("10/02/2025")
+print("P1Hw1")
+print("This assignment is about recreating the output for calculations")
